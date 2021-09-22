6524e44ebf5af4a72feee23b97516a0ebd53bb05
USUARIO-PC\USUARIO <oscarruina@hotmail.com>
2021-09-22 18:25:50 -0300

    intento de carrito

--- pedidosya/src/main/java/com/unla/pedidosya/controllers/CarritoController.java
+++ pedidosya/src/main/java/com/unla/pedidosya/controllers/CarritoController.java
@@ -1,11 +1,7 @@
 package com.unla.pedidosya.controllers;
 
-
-import com.unla.pedidosya.entity.Producto;
 import com.unla.pedidosya.helpers.ViewRouteHelper;
-import com.unla.pedidosya.model.CarritoModel;
 import com.unla.pedidosya.repository.IProductoRepository;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,11 +17,8 @@ public class CarritoController {
 
     @GetMapping("/agregarCarrito/{idProducto}")
     public String agregarCarrito(@PathVariable(name = "idProducto") long idProducto,Model model){
-        CarritoModel carrito = new CarritoModel();
         try{
-            Producto p = repo.findById(idProducto).get();
-            carrito.getProductos().add(p);
-            model.addAttribute("carrito", carrito);
+            model.addAttribute("carrito", repo.findById(idProducto).get());
         }catch(Exception e){
             e.getMessage();
         }
