4a402fec3fa921e7af4a7db98a3d26944a72df51
Oscar Ruina <oscarruina@hotmail.com>
2021-11-03 14:28:08 -0300

    add create table role to database

--- ScriptFinal.sql
+++ ScriptFinal.sql
@@ -1,6 +1,13 @@
 drop database if exists `db-pedidos`;
 create database if not exists `db-pedidos`;
+
 use `db-pedidos`;
+
+create table `roles`(
+  `role_id` int(11) not null auto_increment,
+  `nombre` varchar(50),
+  primary key(`role_id`)
+) engine = InnoDB auto_increment = 1 default charset = latin1;
 
 insert into roles
     (nombre)
